15c35c485993dcfd5dc4a637db620b1ad94c53ae
Eric Austin <eaustin@gnresound.com>
2019-01-24 09:49:56 -0600

    Don't require server selection; instead combine all options together

--- SourceControl.ps1
+++ SourceControl.ps1
@@ -1,8 +1,11 @@
 ﻿
 $location=(Get-Location).Path
 
+#remove previous files
+Remove-Item ".\*.txt" -Exclude "ServersAndDatabases.txt"
+
 #retrieve list of servers and databases from ServersAndDatabases.txt
-$sourcelist=(Import-Csv -LiteralPath ".\ServersAndDatabases.txt")
+$sourcelist=@(Import-Csv -LiteralPath ".\ServersAndDatabases.txt")
 
 #notify and exit if no servers and databases have been added
 If ($sourcelist.Count -eq 0) {
@@ -13,40 +16,26 @@ If ($sourcelist.Count -eq 0) {
     Exit
 }
 
-#if there are single values set the variables to those values
-If (($sourcelist | Measure-Object).Count -eq 1) {$servername=$sourcelist.Server; $databasename=$sourcelist.Database}
-
-#if there are multiple servers and databases display a list to choose from
-If (($sourcelist | Measure-Object).Count -gt 1) {
-$sourcelist | Out-GridView -OutputMode Single -Title "Choose a data source" | ForEach {
+#loop through data sources
+$sourcelist | ForEach {
+    
     $servername=$_.Server
     $databasename=$_.Database
+
+    #build query
+    $eventquery="SELECT [RowID], [EventTimestamp], [Server], [Database], [User], [ObjectName], [ObjectType], [Action], '$databasename' AS 'SourceControlDatabase' FROM SourceControl ORDER BY RowID DESC;"
+
+    $data+=@(Invoke-Sqlcmd $eventquery -ServerInstance $servername -Database $databasename -MaxCharLength 100000 )
+
     }
-}
 
-#exit if no server selected
-If ($servername -eq "" -and $databasename -eq "") {
-    Clear-Host
-    Write-Host "No data source selected. Exiting now."
-    Pause
-    Exit
-}
+#display sourcecontrol results for user selection
+($data | Out-GridView -OutputMode Multiple -Title "Choose two rows") | Sort-Object -Property [0] | ForEach {
+    
+    $filepath=".\" + $_.RowID + ".txt"
 
-Clear-Host
-
-#remove previous files
-Remove-Item ".\*.txt" -Exclude "ServersAndDatabases.txt"
-
-#get data from source control table
-$eventquery="SELECT [RowID], [EventTimestamp], [Server], [Database], [User], [ObjectName], [ObjectType], [Action] FROM SourceControl ORDER BY RowID DESC;"
-#$sqlcommand
-
-(Invoke-Sqlcmd $eventquery -ServerInstance $servername -Database $databasename -MaxCharLength 100000 | Out-GridView -OutputMode Multiple -Title "Choose two rows").RowID |
-Sort-Object -Property [0] | ForEach {
-    $filepath=".\" + $_ + ".txt"
-
-    $definitionquery="SELECT [Definition] FROM SourceControl WHERE RowID=" + $_
-    (Invoke-Sqlcmd $definitionquery -ServerInstance $servername -Database $databasename -MaxCharLength 100000).Definition | Out-File -FilePath $filepath
+    $definitionquery="SELECT [Definition] FROM SourceControl WHERE RowID=" +$_.RowID
+    (Invoke-Sqlcmd $definitionquery -ServerInstance $_.Server -Database $_.SourceControlDatabase -MaxCharLength 100000).Definition | Out-File -FilePath $filepath
 
     Start Notepad++ $filepath
-}
+    }
