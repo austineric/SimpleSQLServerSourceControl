c29b654179ec87dcdca1e3b5af3f417c9d07b083
Eric Austin <eaustin@gnresound.com>
2019-01-24 11:21:20 -0600

    Moved definition files to their own directory

--- SourceControl.ps1
+++ SourceControl.ps1
@@ -1,20 +1,28 @@
 ﻿
 $location=(Get-Location).Path
 
-#remove previous files
-Remove-Item ".\*.txt" -Exclude "ServersAndDatabases.txt"
+Clear-Host
+
+Write-Host ""
+Write-Host "--------------------------------------------------------------------------------------------"
+Write-Host ( "SimpleSQLServerSourceControl - https://github.com/austineric/SimpleSQLServerSourceControl" )
+Write-Host "--------------------------------------------------------------------------------------------"
+Write-Host ""
+Write-Host "Running..."
 
 #retrieve list of servers and databases from ServersAndDatabases.txt
 $sourcelist=@(Import-Csv -LiteralPath ".\ServersAndDatabases.txt")
 
 #notify and exit if no servers and databases have been added
 If ($sourcelist.Count -eq 0) {
-    Clear-Host
+    Write-Host ""
     Write-Host "Please add server and database names to $location\ServersAndDatabases.txt"
+    Write-Host ""
     Write-Host "Exiting now"
+    Write-Host ""
     Pause
     Exit
-}
+    }
 
 #loop through data sources
 $sourcelist | ForEach {
@@ -29,10 +37,17 @@ $sourcelist | ForEach {
 
     }
 
+#check if destination directory exists and create it if it does not
+[boolean]$destination=Test-Path -Path ".\DefinitionFiles"
+If ($destination -eq 0) {New-Item -ItemType Directory -Path ".\DefinitionFiles"}
+
+#remove previous files
+Remove-Item ".\DefinitionFiles\*"
+
 #display sourcecontrol results for user selection
 ($data | Out-GridView -OutputMode Multiple -Title "Choose two rows") | Sort-Object -Property [0] | ForEach {
     
-    $filepath=".\" + $_.RowID + ".txt"
+    $filepath=".\DefinitionFiles\" + $_.RowID + ".txt"
 
     $definitionquery="SELECT [Definition] FROM SourceControl WHERE RowID=" +$_.RowID
     (Invoke-Sqlcmd $definitionquery -ServerInstance $_.Server -Database $_.SourceControlDatabase -MaxCharLength 100000).Definition | Out-File -FilePath $filepath
